5f5373228e2172aa9e51b924219d7cdb99c304fa
Asish Ranjan Das <116077389+imasishranjan@users.noreply.github.com>
2026-03-10 00:33:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Developer", "Photographer", "Singer", "Freelancer","Painter"],
+        strings: ["Developer", "Explorer", "Photographer", "Singer", "Enterpreneur"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student", "Developer", "Photographer", "Singer", "Freelancer","Painter"],
+        strings: ["Developer", "Explorer", "Photographer", "Singer", "Enterpreneur"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -70,4 +70,5 @@ $(document).ready(function(){
             }
         }
     });
+
 });
